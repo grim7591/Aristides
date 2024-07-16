bc1a17f09bf50c8248bae41e903a27d4cfa4f170
grim7591 <agimbelfl@gmail.com>
2024-07-16 16:33:56 -0400

    First Commit, hopefully done right this time

---
+++ PropValModel.py
@@ -0,0 +1,21 @@
+# %% [markdown]
+# # ACPA Property Data Model in Process documentation
+# 
+# **Initial steps**
+# 
+# - Figure out how to make a satisfying markdown document
+# to be able to keep track of progress (Done)
+# - Get data from PACs for model training
+#   - Options:
+#       - Use current year sales to train
+#       - Use time adjusted sales to train
+#           - Time adjust using historical data, might not be too hard
+#       - Use non time adjusted sales and year becomes a factor?
+# - Identify less than 20 features to test for
+#   - Examples from Lee:
+#       - base area, age, 
+#       - improvement code 
+#       - quality, 
+#       - land square ft
+# - Do everything else
+# %%
