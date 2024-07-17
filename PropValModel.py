b59a58c18d37ed6d2afb9c7fd5c151947ec69df1
grim7591 <agimbelfl@gmail.com>
2024-07-17 16:34:35 -0400

    7/17 eod commit, changes to outline

--- PropValModel.py
+++ PropValModel.py
@@ -8,8 +8,10 @@
 # - Get data from PACs for model training
 #   - Options:
 #       - Use current year sales to train
+#           - Probably not the move but might not be a bad place to start.
 #       - Use time adjusted sales to train
-#           - Time adjust using historical data, might not be too hard
+#           - Time adjust using historical data, might not be too hard.
+#           - One year of sales probably isn't enough because it biases the model towards 2023-like market conditions
 #       - Use non time adjusted sales and year becomes a factor?
 # - Identify less than 20 features to test for
 #   - Examples from Lee:
