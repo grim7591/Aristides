7d9bf56e9f4071548b7e63bff7c2f4e5eb81a4e5
grim7591 <agimbelfl@gmail.com>
2024-07-30 11:02:07 -0400

    branch1

--- CleanedUpModel.py
+++ CleanedUpModel.py
@@ -7,7 +7,7 @@ import seaborn as sns
 market_area_1 = pd.read_csv('MA1_Export_20240719.csv')
 market_area_2 = pd.read_csv('MA2_Export_20240719.csv')
 market_area_3 = pd.read_csv('MA3_Export_20240719.csv')
-data_2 = pd.read_csv("datapull3.csv")
+data_2 = pd.read_csv("datapull5.csv")
 # %%
 market_area_1 = market_area_1[['prop_id', 'Cluster_ID']]
 market_area_2 = market_area_2[['prop_id', 'Cluster_ID']]
@@ -27,7 +27,7 @@ result['sl_price'] = np.log(result['sl_price'] + 1)
 result['living_area'] = np.log(result['living_area'] + 1)
 result['yr_blt'] = np.log(result['yr_blt'] + 1)
 # %%
-result = result.join(pd.get_dummies(result.city_id)).drop(['city_id'], axis=1)
+result = result.join(pd.get_dummies(result.tax_area_description)).drop(['tax_area_description'], axis=1)
 # %%
 result = result.join(pd.get_dummies(result.land_type_cd)).drop(['land_type_cd'], axis=1)
 # %%
