5bdcf4fa21c22c3e955647681144e989be51f06f
grim7591 <agimbelfl@gmail.com>
2025-02-11 16:41:32 -0500

    2/11 eod

--- ModelV4.py
+++ ModelV4.py
@@ -63,24 +63,15 @@ from PlotPlotter import PlotPlotter
 import matplotlib.pyplot as plt
 from IPython.display import Markdown
 
-# Import Libraries 
-import numpy as np
-import pandas as pd
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from IAAOFunctions import PRD, COD, PRB, weightedMean, averageDeviation
-from StrataCaster import StrataCaster
-from PlotPlotter import PlotPlotter
-import matplotlib.pyplot as plt
-
 # Load the data
 print("Loading data from CSV file...")
-result = pd.read_csv('Data/dp70m.csv')
+result = pd.read_csv('Data/dp70m3.csv')
 #result = pd.read_csv('Data/dp2124c.csv')
 result.rename(columns={'Name': 'Market_Cluster_ID'}, inplace=True)
+flood_zones = pd.read_csv('Data/Prop_FloodPlains.csv')
+flood_zones['prop_id'] = flood_zones['prop_id'].astype(str)
+result['prop_id'] = result['prop_id'].astype(str)
+result = result.merge(flood_zones[['prop_id', 'FLD_ZONE']], on='prop_id', how='left')
 '''
 result['Market_Cluster_ID'] = result.apply(
     lambda row: 'Highland' if row['hood_cd'] in [324428.02, 324428.01] else row['Market_Cluster_ID'], axis=1
@@ -379,7 +370,9 @@ result.columns = result.columns.astype(str)
 # regressionFormula = "np.log(Assessment_Val) ~ np.log(living_area) + np.log(landiness) + np.log(percent_good) + np.log(imprv_det_quality_cd) + np.log(total_porch_area + 1) + np.log(total_garage_area + 1) + Springtree_B + HighSprings_A + MidtownEast_C + swNewberry_B + MidtownEast_A + swNewberry_A + MidtownEast_B + HighSprings_F + Springtree_A + Tioga_B + Tioga_A + MidtownEast_D + WaldoRural_A + Alachua_Main + High_Springs_Main + HaileLike + HighSprings_B + Real_Tioga + Duck_Pond + Newmans_Lake + EastMidtownEastA + HighSpringsAGNV + Hawthorne + HighSprings_B + Golfview + Lugano + Archer + WildsPlantation+Buck_Bay+in_subdivision+has_lake+WaldoRural_C+HighSprings_E+HSBUI+number_of_baths+EastGNV+Ironwood+SummerCreek+has_canal+TC_Forest+CarolEstates+Westchesterish+QuailCreekish"
 
 # %%
-regressionFormula = "np.log(Assessment_Val) ~ np.log(living_area) + np.log(landiness) + np.log(percent_good) + np.log(imprv_det_quality_cd) + np.log(total_porch_area + 1) + np.log(total_garage_area + 1) + number_of_baths + in_subdivision + Alachua_Main + Archer + Buck_Bay + Carol_Estates + Duck_Pond + EastGNV + EastMidtownEastA + Golfview + Haile_Like + Hawthorne + Hickory_Forest + High_Springs_Main + HighSpringsA + HighSpringsAGNV + HighSpringsAGNVSouth + HighSpringsB+HSBUI_2 + Ironwood + Jonesville + Kanapaha + Lincoln_Estates + Lugano + MidtownEast_A + MidtownEast_B + MidtownEast_C + Montery  + Newnans_Lake + QuailCreek + Rural_North + Rural_South + San_Felasco + Sorrento + Split_Rock + Springtree + SummerCreek + Sweetwater + swNewberry_A + swNewberry_B + TC_Forest + Tioga + Tioga_A + Tioga_B + Turkey_Creek + Valwood + Waldo + WaldoRural + Westchester + WildsPlantation+Highland"
+#regressionFormula = "np.log(Assessment_Val) ~ np.log(living_area) + np.log(landiness) + np.log(percent_good) + np.log(imprv_det_quality_cd) + np.log(total_porch_area + 1) + np.log(total_garage_area + 1) + number_of_baths + in_subdivision + Alachua_Main + Archer + Buck_Bay + Carol_Estates + Duck_Pond + EastGNV + EastMidtownEastA + Golfview + Haile_Like + Hawthorne + Hickory_Forest + High_Springs_Main + HighSpringsA + HighSpringsAGNV + HighSpringsAGNVSouth + HighSpringsB+Florida_Park + Ironwood + Jonesville + Kanapaha + Lincoln_Estates + Lugano + MidtownEast_A + MidtownEast_B + MidtownEast_C + Montery  + Newnans_Lake + QuailCreek + Rural_South + San_Felasco + Sorrento + Split_Rock + Springtree + SummerCreek + Sweetwater + swNewberry_A + swNewberry_B + TC_Forest + Tioga + Tioga_A + Tioga_B + Turkey_Creek + Valwood + Waldo + WaldoRural + Westchester + WildsPlantation+Highland"
+
+regressionFormula = "np.log(Assessment_Val) ~ np.log(living_area) + np.log(landiness) + np.log(percent_good) + np.log(imprv_det_quality_cd) + np.log(total_porch_area + 1) + np.log(total_garage_area + 1) + number_of_baths + in_subdivision + C(Market_Cluster_ID)"
 # %% [markdown]
 # ### Train/Test Split
 # The data is split into training and testing sets. The training data is used to inform the model, the test data is used to check the performance of the trained model. The split takes out a random 20% of the properties to use for testing but for my purposes I've been using the same random seed so that variation in the results is from changes I make to the model and not from just getting a different seed. I believe the plan in the future will be to run the model on multiple seeds.
@@ -767,7 +760,7 @@ outliers_df.to_csv('3IQR.csv')
 
 # %%
 # PREDICTIONS
-pop_data = pd.read_csv('Data/bdp70m.csv')
+pop_data = pd.read_csv('Data/bdp70m3.csv')
 pop_data.rename(columns={'Name': 'Market_Cluster_ID'}, inplace=True)
 pop_data = pop_data.join(pd.get_dummies(pop_data.tax_area_description))
 pop_data = pop_data.join(pd.get_dummies(pop_data.Market_Cluster_ID))
@@ -818,7 +811,6 @@ pop_data['imprv_det_quality_cd'] = pop_data['imprv_det_quality_cd'].replace({
 })
 
 pop_data['predicted_log_Assessment_Val'] = regresult.predict(pop_data)
-# %%
 # Convert predicted log values to original scale
 pop_data['predicted_Assessment_Val'] = np.exp(pop_data['predicted_log_Assessment_Val'])
 # Calculate predicted market value by adding miscellaneous value
@@ -837,24 +829,17 @@ pop_data['sale_ratio'] = pop_data['predicted_Market_Val'] / pop_data['market']
 # Export pop_data to CSV
 print("Exporting geospatial analysis data to CSV...")
 pop_data.to_csv('pop_data_predicted.csv', index=False)
-# %%
 pop_data['predicted_Market_Val'].describe()
-# %%
 pop_data['market'].describe()
-# %%
 pop_data['predicted_Market_Val'] = pd.to_numeric(pop_data['predicted_Market_Val'], errors='coerce')
-# %%
 summary_df = pd.DataFrame({
     'predicted_Market_Val': pop_data['predicted_Market_Val'].describe(),
     'market': pop_data['market'].describe()
 })
-
 # Add a total row
 summary_df.loc['total'] = [pop_data['predicted_Market_Val'].sum(), pop_data['market'].sum()]
-
 # Format numbers with commas and two decimal places
 summary_df = summary_df.map(lambda x: f"{x:,.2f}")
-
 print(summary_df)
 # %%
 # Group by 'hood_cd', summing both columns and adding a count of properties
@@ -870,10 +855,134 @@ hood_comparison['pct_difference'] = (
 ) * 100  # Convert to percentage
 
 # Save to CSV for easier viewing
-hood_comparison.to_csv("hood_comparison_2.csv", index=False)
+hood_comparison.to_csv("hood_comparison_4.csv", index=False)
 
 # Display the DataFrame summary
 print(hood_comparison)
 print(hood_comparison['pct_difference'].describe())
 
+# %%
+# 2024 only predictions
+# PREDICTIONS
+twofour_data = pd.read_csv('Data/24sales.csv')
+twofour_data.rename(columns={'Name': 'Market_Cluster_ID'}, inplace=True)
+twofour_data = twofour_data.join(pd.get_dummies(twofour_data.tax_area_description))
+twofour_data = twofour_data.join(pd.get_dummies(twofour_data.Market_Cluster_ID))
+twofour_data.drop(twofour_data[twofour_data['legal_acreage'] >= 1].index, inplace=True)
+twofour_data.drop(twofour_data[twofour_data['Market_Cluster_ID'] == 'Rural_North'].index, inplace=True)
+twofour_data.drop(twofour_data[twofour_data['Join_Count'] != 1].index, inplace=True)
+
+# Factor engineer "landiness"
+print("Calculating landiness...")
+# Calculate the average legal acreage in square feet
+avg_legal_acreage = (twofour_data['legal_acreage'] * 43560).mean()
+# Create 'landiness' as a ratio of property acreage to average acreage
+twofour_data['landiness'] = (twofour_data['legal_acreage'] * 43560) / avg_legal_acreage
+# ### Creating in_subdivision
+# Binary variable for if a property is in a subdivision or not.
+
+# Make subdivision code binary variable
+print("Creating binary variables for subdivision status...")
+# Create a binary variable 'in_subdivision' to indicate if property is in a subdivision
+twofour_data['in_subdivision'] = twofour_data['abs_subdv_cd'].apply(lambda x: True if x > 0 else False)
+# Drop unnecessary columns
+twofour_data = twofour_data.drop(columns=['abs_subdv_cd'])
+
+# Convert 'prop_id' to string for consistency across dataframes
+twofour_data['prop_id'] = twofour_data['prop_id'].astype(str)
+
+# ### Effective age overwrites
+# In 2024 we updated the effective year built of all properties to 1994 at minimum. When reviewing outliers I applied that same logic to these properties which were evaluated on pre-2024 factors. It was determined by valuation that a 30 year limit on effective age makes sense and because that was a change in our process and not necessarily a market shift, I think it makes sense to mitigate the impact of that change on the model by applying it retroactively to previous sale years. 
+
+twofour_data['effective_age'] = twofour_data['effective_age'].apply(lambda x: 30 if x > 30 else x)
+
+# ### Calculating "percent good" from effective age
+# Percent good = 1 - (effective_age/100)
+# Factor Engineer Percent Good based on effective age
+print("Calculating percent good based on effective age...")
+# Calculate 'percent_good' as a factor of effective age
+twofour_data['percent_good'] = 1 - (twofour_data['effective_age']/ 100)
+# Linearize the quality codes
+print("Linearizing quality codes...")
+# Replace quality codes with numerical values for linear regression
+twofour_data['imprv_det_quality_cd'] = twofour_data['imprv_det_quality_cd'].replace({
+    1: 0.1331291,
+    2: 0.5665645,
+    3: 1.0,
+    4: 1.1624432,
+    5: 1.4343298,
+    6: 1.7062164
+})
+
+twofour_data['predicted_log_Assessment_Val'] = regresult.predict(twofour_data)
+
+print("Evaluating model performance on test data...")
+# Get predictions to test
+predictions_2 = twofour_data.copy()
+# Predict log-transformed assessment values
+predictions_2['predicted_log_Assessment_Val'] = regresult.predict(predictions_2)
+# Convert predicted log values to original scale
+predictions_2['predicted_Assessment_Val'] = np.exp(predictions_2['predicted_log_Assessment_Val'])
+# Define actual and predicted values for further evaluation
+actual_values = predictions_2['sl_price']
+predicted_values = predictions_2['predicted_Assessment_Val'] + predictions_2['MISC_Val']
+predicted_values_mae = predictions_2['predicted_Assessment_Val']
+predictions_2['Assessment_Val'] = .85 * (predictions_2['sl_price'] - (predictions_2['MISC_Val'] / .85))
+actual_values_mae = predictions_2['Assessment_Val']
+
+# Test predictions on performance metrics
+print("Calculating performance metrics...")
+mae = mean_absolute_error(predicted_values, actual_values)
+mae_2 = mean_absolute_error(predicted_values_mae, actual_values_mae)
+# Calculate IAAO metrics
+PRD_table = PRD(predicted_values, actual_values)
+COD_table = COD(predicted_values, actual_values)
+PRB_table = PRB(predicted_values, actual_values)
+PRBCI_table = PRBCI(predicted_values, actual_values)
+wm = weightedMean(predicted_values, actual_values)
+meanRatio = (predicted_values / actual_values).mean()
+medianRatio = (predicted_values / actual_values).median()
+
+PRD_table_2 = PRD(predicted_values_mae, actual_values_mae)
+COD_table_2 = COD(predicted_values_mae, actual_values_mae)
+PRB_table_2 = PRB(predicted_values_mae, actual_values_mae)
+PRBCI_table_2 = PRBCI(predicted_values_mae, actual_values_mae)
+wm_2 = weightedMean(predicted_values_mae, actual_values_mae)
+meanRatio_2 = (predicted_values_mae / actual_values_mae).mean()
+medianRatio_2 = (predicted_values_mae / actual_values_mae).median()
+
+# Print performance metrics
+print(f"Mean Absolute Error: {mae}")
+print(f"Mean Absolute Error_2: {mae_2}")
+print(f"PRD: {PRD_table}")
+print(f"COD: {COD_table}")
+print(f"PRB: {PRB_table}")
+print(f"PRBCI: {PRBCI_table}")
+print(f"weightedMean: {wm}")
+print(f"meanRatio: {meanRatio}")
+print(f"medianRatio: {medianRatio}")
+
+''' print(f"PRD: {PRD_table_2}")
+print(f"COD: {COD_table_2}")
+print(f"PRB: {PRB_table_2}")
+print(f"PRBCI: {PRBCI_table_2}")
+print(f"weightedMean: {wm_2}")
+print(f"meanRatio: {meanRatio_2}")
+print(f"medianRatio: {medianRatio_2}") '''
+
+# Convert predicted log values to original scale
+twofour_data['predicted_Assessment_Val'] = np.exp(twofour_data['predicted_log_Assessment_Val'])
+# Calculate predicted market value by adding miscellaneous value
+twofour_data['predicted_Market_Val'] = twofour_data['predicted_Assessment_Val'] + twofour_data['MISC_Val']
+# Calculate residuals for market and assessment values
+twofour_data['Market_Residual'] = twofour_data['predicted_Market_Val'] - twofour_data['sl_price']
+#twofour_data['Assessment_Residual'] = twofour_data['predicted_Assessment_Val'] - twofour_data['Assessment_Val']
+# Convert residuals to numeric and handle errors
+twofour_data['Market_Residual'] = pd.to_numeric(twofour_data['Market_Residual'], errors='coerce')
+#twofour_data['Assessment_Residual'] = pd.to_numeric(twofour_data['Assessment_Residual'], errors='coerce')
+# Calculate absolute values of residuals
+twofour_data['AbsV_Market_Residual'] = twofour_data['Market_Residual'].abs()
+#twofour_data['AbsV_Assessment_Residual'] = twofour_data['Assessment_Residual'].abs()
+# Calculate sale ratio
+twofour_data['sale_ratio'] = twofour_data['predicted_Market_Val'] / twofour_data['sl_price']
 # %%
